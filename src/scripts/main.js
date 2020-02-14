fc2e321a38b8ebaf198d5cf375237bae17f262da
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-14 16:37:39 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,41 @@
 'use strict';
 
 const scale = document.querySelector('.slider__scale');
-const minToggle = scale.querySelector('.slider__toggle--min');
-const maxToggle = scale.querySelector('.slider__toggle--max');
+const minThumb = scale.querySelector('.slider__thumb--min');
+const maxThumb = scale.querySelector('.slider__thumb--max');
 const inputMin = document.querySelector('.slider__input--min');
 const inputMax = document.querySelector('.slider__input--max');
+const tooltipMin = document.querySelector('.slider__tooltip--min');
+const tooltipMax = document.querySelector('.slider__tooltip--max');
+
 const lineDiff = scale.querySelector('.slider__diff-line');
-const toggleWidth = minToggle.offsetWidth;
+const thumbWidth = minThumb.offsetWidth;
 
-minToggle.style.left = '50px';
-maxToggle.style.left = '300px';
+minThumb.style.left = '50px';
+maxThumb.style.left = '300px';
 
-inputMin.value = parseInt(minToggle.style.left);
-inputMax.value = parseInt(maxToggle.style.left);
+inputMin.value = parseInt(minThumb.style.left);
+inputMax.value = parseInt(maxThumb.style.left);
+tooltipMin.textContent = parseInt(minThumb.style.left);
+tooltipMax.textContent = parseInt(maxThumb.style.left);
 
-let diffToggles;
+let diffThumbs;
 
-diffToggles = parseInt(maxToggle.style.left) - parseInt(minToggle.style.left);
+diffThumbs = parseInt(maxThumb.style.left) - parseInt(minThumb.style.left);
 
-lineDiff.style.width = `${diffToggles}px`;
-lineDiff.style.left = minToggle.style.left;
+lineDiff.style.width = `${diffThumbs}px`;
+lineDiff.style.left = minThumb.style.left;
 
 const diffCounter = (min, max) => {
-  diffToggles = parseInt(max.style.left) - parseInt(min.style.left);
-  lineDiff.style.width = `${diffToggles}px`;
+  diffThumbs = parseInt(max.style.left) - parseInt(min.style.left);
+  lineDiff.style.width = `${diffThumbs}px`;
   lineDiff.style.left = min.style.left;
 };
 
 const onClickHandler = (evt) => {
   const scalePosition = scale.getBoundingClientRect();
   const scaleX = scalePosition.left + scale.clientLeft;
-  let clickPoint = evt.clientX - scaleX - toggleWidth / 2;
+  let clickPoint = evt.clientX - scaleX - thumbWidth / 2;
 
   if (clickPoint < 0) {
     clickPoint = 0;
@@ -40,41 +45,43 @@ const onClickHandler = (evt) => {
     return;
   }
 
-  const distanseToMin = Math.abs(parseInt(minToggle.style.left) - clickPoint);
-  const distanseToMax = Math.abs(parseInt(maxToggle.style.left) - clickPoint);
+  const distanseToMin = Math.abs(parseInt(minThumb.style.left) - clickPoint);
+  const distanseToMax = Math.abs(parseInt(maxThumb.style.left) - clickPoint);
 
   if (distanseToMin <= distanseToMax) {
-    minToggle.style.left = `${clickPoint}px`;
-    inputMin.value = Math.round(parseInt(minToggle.style.left));
+    minThumb.style.left = `${clickPoint}px`;
+    inputMin.value = parseInt(minThumb.style.left) + thumbWidth / 2;
+    tooltipMin.textContent = parseInt(minThumb.style.left) + thumbWidth / 2;
   } else {
-    maxToggle.style.left = `${clickPoint}px`;
-    inputMax.value = parseInt(maxToggle.style.left) + toggleWidth / 2;
+    maxThumb.style.left = `${clickPoint}px`;
+    inputMax.value = parseInt(maxThumb.style.left) + thumbWidth / 2;
+    tooltipMax.value = parseInt(maxThumb.style.left) + thumbWidth / 2;
   }
 
-  diffCounter(minToggle, maxToggle);
+  diffCounter(minThumb, maxThumb);
 };
 
 // limits
 
-const limitMinToggle = function(left) {
-  if ((left < -toggleWidth / 2)
-  || (left > parseInt(maxToggle.style.left) - toggleWidth)) {
+const limitMinThumb = function(left) {
+  if ((left < -thumbWidth / 2)
+  || (left > parseInt(maxThumb.style.left) - thumbWidth)) {
     return true;
   }
 
   return false;
 };
 
-const limitMaxToggle = function(left) {
-  if ((left < parseInt(minToggle.style.left) + toggleWidth)
-  || (left > scale.offsetWidth - maxToggle.offsetWidth)) {
+const limitmaxThumb = function(left) {
+  if ((left < parseInt(minThumb.style.left) + thumbWidth)
+  || (left > scale.offsetWidth - maxThumb.offsetWidth)) {
     return true;
   }
 
   return false;
 };
 
-// moveMinToggle
+// moveMinThumb
 
 const removeListeners = (move, up) => {
   document.removeEventListener('mousemove', move);
@@ -86,29 +93,30 @@ const addListeners = (move, up) => {
   document.addEventListener('mouseup', up);
 };
 
-const moveMinToggle = (downEvt) => {
-  downEvt.preventDefault();
+const moveMinThumb = (downEvent) => {
+  downEvent.preventDefault();
 
-  let startPoint = downEvt.clientX;
+  let startPoint = downEvent.clientX;
 
-  const onMouseMove = (moveEvt) => {
-    moveEvt.preventDefault();
+  const onMouseMove = (moveEvent) => {
+    moveEvent.preventDefault();
 
-    const distance = startPoint - moveEvt.clientX;
+    const distance = startPoint - moveEvent.clientX;
 
-    if (limitMinToggle(minToggle.offsetLeft - distance)) {
+    if (limitMinThumb(minThumb.offsetLeft - distance)) {
       return;
     }
 
-    startPoint = moveEvt.clientX;
-    minToggle.style.left = `${minToggle.offsetLeft - distance}px`;
-    inputMin.value = parseInt(minToggle.style.left) + toggleWidth / 2;
+    startPoint = moveEvent.clientX;
+    minThumb.style.left = `${minThumb.offsetLeft - distance}px`;
+    inputMin.value = parseInt(minThumb.style.left) + thumbWidth / 2;
+    tooltipMin.textContent = parseInt(minThumb.style.left) + thumbWidth / 2;
 
-    diffCounter(minToggle, maxToggle);
+    diffCounter(minThumb, maxThumb);
   };
 
-  const onMouseUp = (upEvt) => {
-    upEvt.preventDefault();
+  const onMouseUp = (upEvent) => {
+    upEvent.preventDefault();
 
     removeListeners(onMouseMove, onMouseUp);
   };
@@ -116,32 +124,33 @@ const moveMinToggle = (downEvt) => {
   addListeners(onMouseMove, onMouseUp);
 };
 
-// moveMaxToggle
+// movemaxThumb
 
-const moveMaxToggle = (downEvt) => {
-  downEvt.preventDefault();
-  maxToggle.style.transition = 'none';
+const movemaxThumb = (downEvent) => {
+  downEvent.preventDefault();
+  maxThumb.style.transition = 'none';
 
-  let startPoint = downEvt.clientX;
+  let startPoint = downEvent.clientX;
 
-  const onMouseMove = (moveEvt) => {
-    moveEvt.preventDefault();
+  const onMouseMove = (moveEvent) => {
+    moveEvent.preventDefault();
 
-    const distance = startPoint - moveEvt.clientX;
+    const distance = startPoint - moveEvent.clientX;
 
-    if (limitMaxToggle(maxToggle.offsetLeft - distance)) {
+    if (limitmaxThumb(maxThumb.offsetLeft - distance)) {
       return;
     }
 
-    startPoint = moveEvt.clientX;
-    maxToggle.style.left = `${maxToggle.offsetLeft - distance}px`;
-    inputMax.value = parseInt(maxToggle.style.left) + toggleWidth / 2;
+    startPoint = moveEvent.clientX;
+    maxThumb.style.left = `${maxThumb.offsetLeft - distance}px`;
+    inputMax.value = parseInt(maxThumb.style.left) + thumbWidth / 2;
+    tooltipMax.textContent = parseInt(maxThumb.style.left) + thumbWidth / 2;
 
-    diffCounter(minToggle, maxToggle);
+    diffCounter(minThumb, maxThumb);
   };
 
-  const onMouseUp = (upEvt) => {
-    upEvt.preventDefault();
+  const onMouseUp = (upEvent) => {
+    upEvent.preventDefault();
 
     removeListeners(onMouseMove, onMouseUp);
   };
@@ -151,24 +160,32 @@ const moveMaxToggle = (downEvt) => {
 
 // inputEvents
 
-const minInputHandler = (inputEvt) => {
-  if (inputMin.value > inputMax.value - toggleWidth) {
-    inputMin.value = inputMax.value - toggleWidth;
+const minInputHandler = (inputEvent) => {
+  if (inputMin.value < 0) {
+    inputMin.value = 0;
   }
 
-  minToggle.style.left = `${inputMin.value}px`;
-  diffCounter(minToggle, maxToggle);
+  if (inputMin.value > inputMax.value - thumbWidth) {
+    inputMin.value = inputMax.value - thumbWidth;
+  }
+
+  minThumb.style.left = `${inputMin.value}px`;
+  diffCounter(minThumb, maxThumb);
 };
 
-const maxInputHandler = (inputEvt) => {
-  maxToggle.style.left = `${inputMax.value}px`;
-  diffCounter(minToggle, maxToggle);
+const maxInputHandler = (inputEvent) => {
+  if (inputMax.value > 500) {
+    inputMax.value = 500;
+  }
+
+  maxThumb.style.left = `${inputMax.value}px`;
+  diffCounter(minThumb, maxThumb);
 };
 
 inputMin.addEventListener('input', debounce(minInputHandler));
 inputMax.addEventListener('input', debounce(maxInputHandler));
-minToggle.addEventListener('mousedown', moveMinToggle);
-maxToggle.addEventListener('mousedown', moveMaxToggle);
+minThumb.addEventListener('mousedown', moveMinThumb);
+maxThumb.addEventListener('mousedown', movemaxThumb);
 scale.addEventListener('click', onClickHandler);
 
 function debounce(callback) {
@@ -181,6 +198,6 @@ function debounce(callback) {
 
     lastTimeout = setTimeout(() => {
       callback.apply(null, arguments);
-    }, 1000);
+    }, 500);
   };
 };
